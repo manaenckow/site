5afe76a3dc98e40ef1cd3c152f5b117acd8f027b
Maxim <48844646+manaenckow@users.noreply.github.com>
2020-03-06 00:28:58 +0300

    Updates

---
+++ precache-manifest.87ee13c7ce9cc3e3e5a3639085c08f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b84be619ad4ce0aaa8d37b741077d0d",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "656ebe548b2dd9e40b9b",
+    "url": "/site/static/css/main.0433e9c5.chunk.css"
+  },
+  {
+    "revision": "fa4c6d64b79fdee3f2d8",
+    "url": "/site/static/js/2.26a432e0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/site/static/js/2.26a432e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656ebe548b2dd9e40b9b",
+    "url": "/site/static/js/main.5957a0e1.chunk.js"
+  },
+  {
+    "revision": "534eef13f5100a98c8ea",
+    "url": "/site/static/js/runtime-main.1353ce0c.js"
+  }
+]);
